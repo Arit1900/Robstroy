1eb6a76d10ee43993d55e01f4da56d9d4634317d
pop <rarit@hotmail.com>
2017-11-15 20:02:53 +0700

    Initial

--- index.php
+++ index.php
@@ -7,46 +7,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token =
 '1caj07yURrrjtw3H9hzJ+bWYo+rsam3iGzbrYrrRhjH7zZqHuz3Dujm0jtJsVTThFpev/g0oK8CZYAON18xdMenFO+a02ALh4r2OlXPFyPsBscfr7b4IZwNoBOVLfS6RUA70p2pIWk3dpq+U8aHwtQdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '5e8d1f791608a58b43c5bd31c986d955';
-
-$logger = new Logger('LineBot');
-$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
-
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-try {
-  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
-  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
-  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
-}
-foreach ($events as $event) {
-  // Postback Event
-  if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
-    $logger->info('Postback message has come');
-    continue;
-  }
-  // Location Event
-  if  ($event instanceof LINE\LINEBot\Event\MessageEvent\LocationMessage) {
-    $logger->info("location -> ".$event->getLatitude().",".$event->getLongitude());
-    continue;
-  }
-  
-  // Message Event = TextMessage
-  if (($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-    // get message text
-    $messageText=strtolower(trim($event->getText()));
-    
-  }
-}  
-
-$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("text message");
-$bot->replyMessage($event->getReplyToken(), $outputText);
-
-/*// Get message from Line API
+// Get message from Line API
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 if (!is_null($events['events'])) {
@@ -70,12 +31,34 @@ $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
 $textMessageBuilder = new TextMessageBuilder($respMessage);
 $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 // Reply message
-$respMessage = 'Hello, your message is '. $event['message']['text'];
+//$respMessage = 'Hello, your message is '. $event['message']['text'];
 $httpClient = new CurlHTTPClient($channel_token);
 $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
 $textMessageBuilder = new TextMessageBuilder($respMessage);
 $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 break;
+
+
+
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+}
+foreach ($events as $event) {
+  // Postback Event
+  if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
+    $logger->info('Postback message has come');
+    continue;
+  }
+
+
+
 }
 }
 }

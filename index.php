d1a4dc8e6691f2b70bcf80fe4451bb3090a5cb81
pop <rarit@hotmail.com>
2017-11-15 18:57:31 +0700

    Initial...

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token =
-'1caj07yURrrjtw3H9hzJ+bWYo+rsam3iGzbrYrrRhjH7zZqHuz3Dujm0jtJsVTThFpev/g0oK8CZYAON18xdMenFO+a02ALh4r2OlXPFyPsBscfr7b4IZwNoBOVLfS6RUA70p2pIWk3dpq+U8aHwtQdB04t89/1O/w1cDnyilFU=';
-$channel_secret = '5e8d1f791608a58b43c5bd31c986d955';
+'1v2OUa9tuMIiDhEg57ANbsRaBDbBGP9nlCC+Dpvt5HrsQ+LqcrImWPUBkH8re/pwqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLr
+SkLdcjbFurwXGOyHLt8MdgzLfAe7r0BsQV5cATlUanW3OgJewdB04t89/1O/w1cDnyilFU=';
+$channel_secret = '9b2c7349ea939ef723a3cb453d774c86';
 // Get message from Line API
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
@@ -14,22 +15,11 @@ if (!is_null($events['events'])) {
 // Loop through each event
 foreach ($events['events'] as $event) {
 // Line API send a lot of event type, we interested in message only.
-/*if ($event['type'] == 'message') {
+if ($event['type'] == 'message') {
 switch($event['message']['type']) {
-case 'text':*/
+case 'text':
 // Get replyToken
 $replyToken = $event['replyToken'];
-switch($event['message']['type']) {
-case 'image':
-$messageID = $event['message']['id'];
-$respMessage = 'Hello, your image ID is '. $messageID;
-break;
-default:
-}
-$httpClient = new CurlHTTPClient($channel_token);
-$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-$textMessageBuilder = new TextMessageBuilder($respMessage);
-$response = $bot->replyMessage($replyToken, $textMessageBuilder);
 // Reply message
 $respMessage = 'Hello, your message is '. $event['message']['text'];
 $httpClient = new CurlHTTPClient($channel_token);

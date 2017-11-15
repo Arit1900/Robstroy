035920e0c81085f7b60a762cb36bcd436354cfd9
pop <rarit@hotmail.com>
2017-11-15 20:16:09 +0700

    Initial

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token =
 '1caj07yURrrjtw3H9hzJ+bWYo+rsam3iGzbrYrrRhjH7zZqHuz3Dujm0jtJsVTThFpev/g0oK8CZYAON18xdMenFO+a02ALh4r2OlXPFyPsBscfr7b4IZwNoBOVLfS6RUA70p2pIWk3dpq+U8aHwtQdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '5e8d1f791608a58b43c5bd31c986d955';
-// Get message from Line API
+/*// Get message from Line API
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 if (!is_null($events['events'])) {
@@ -73,7 +73,7 @@ foreach ($events as $event) {
 }  
 
 
-  
+
 }
 }
 }
